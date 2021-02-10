d37c439b19915dea351057e7101fe90b1d2e765d
Adil Haydar <47712899+AdilHaydar@users.noreply.github.com>
2021-02-10 16:23:37 +0300

    Update is_perm_allowed.py

--- user/templatetags/is_perm_allowed.py
+++ user/templatetags/is_perm_allowed.py
@@ -3,15 +3,36 @@ from django import template
 
 register = template.Library()
 
+@register.filter(name='isAuth')
+def isAuth(user,process):
+    if user.group:
+        user_perms = user.group.permissions.values_list('codename',flat=True)
+        if process in user_perms:
+            return True
+    
+    else:
+        return False
+
+@register.filter(name='is_selected')
+def is_selected(foo,bar):
+    foo = str(foo)
+    bar = str(bar)
+
+    if foo==bar:
+        return True
+    return False
+
 @register.filter(name='is_authorized')
 def is_authorized(user,perm):
+    if user.is_superuser:
+        return True
     if user.group:
         user_perms = user.group.permissions.values_list('codename',flat=True)
         if perm in user_perms:
             return True
     else:
         return False
-    
+
 @register.filter(name='dropdown_filter')
 def dropdown_filter(user,perms):
     if user.is_superuser:
